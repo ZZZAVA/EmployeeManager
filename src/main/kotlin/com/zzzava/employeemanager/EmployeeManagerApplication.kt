5108bab9050f548297a6dbcf9816a7dfd7b7f28d
ZZZAVA <work.zava@gmail.com>
2024-02-09 22:19:35 +0300

    Initial commit

---
+++ src/main/kotlin/com/zzzava/employeemanager/EmployeeManagerApplication.kt
@@ -0,0 +1,12 @@
+package com.zzzava.employeemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeManagerApplication
+
+fun main(args: Array<String>) {
+
+    runApplication<EmployeeManagerApplication>(*args)
+}
